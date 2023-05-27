1f75f669b25080f7e6edfbeaecb67f29609e6d90
sonu25122000 <skg74827@gmail.com>
2023-05-28 04:29:08 +0530

    frontend project iniltilized

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Navbar from "./pages/Navbar";
+import { Allroute } from "./pages/allroute";
+
+function App() {
+  return (
+    <div>
+      bookstore
+    </div>
+  );
+}
+
+export default App;
